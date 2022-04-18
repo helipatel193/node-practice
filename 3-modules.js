391b60a19d0b8068eacdb5a438c8f7d6ef153a41
helipatel193 <helipatel193@gmail.com>
2022-04-18 17:42:01 +0530

    Initial commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+// CommonnJS, every file is module (by default)
+// Modules - Encapsulated code (only share minimum)
+
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+sayHi('heli')
+sayHi(names.john)
+sayHi(names.peter)
